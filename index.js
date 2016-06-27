e2ee5357684a3e4d05b977520f3be826791a9514
Erick Fernando Santiago Soto <ferso@Ericks-MacBook-Pro.local>
2016-06-26 20:15:28 -0500

    tenant services capabilities

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express 	 	= require('express');
 const bodyParser 	= require('body-parser');
 const path    	 	= require('path');
 const fs     	 	= require('fs');
-const cors   		= require('cors')  
+const cors   		= require('cors')
 const router 		= express.Router()
 const extend 	 	= require('util')._extend;
 const join   	 	= require('path').join;
@@ -14,7 +14,7 @@ const eh 			= require('errorhandler');
 const multer 		= require('multer');
 const _ 			= require("underscore");
 const osTmpdir 		= require('os-tmpdir');
-const dmn      		= require('domain').create(); 
+const dmn      		= require('domain').create();
 
 
 "use strict";
@@ -29,14 +29,14 @@ dmn.on('error', function(error,r){
 
 
 //Create APP
-const app    	= express();  
+const app    	= express();
 const http      = require('http').Server(app);
 const io        = require('socket.io')(http);
-	  
-// Routes 
+
+// Routes
 // ----------------------------------------------
 const connections 	= require( join(fs.realpathSync('config/',{}), '/connections.js'));
-const model  		= require('./lib/'.concat(connections[connections.default].adapter,'.js')); 
+const model  		= require('./lib/'.concat(connections[connections.default].adapter,'.js'));
 const routes 		= require( join(fs.realpathSync('config/',{}), '/routes.js'));
 const local 		= require( join(fs.realpathSync('config/',{}), '/local.js'));
 const sessconf 		= require( join(fs.realpathSync('config/',{}), '/sessions.js')).session;
@@ -61,7 +61,7 @@ app.use(bodyParser.urlencoded({ extended: true, limit: upload_limit, parameterLi
 app.use(multer({dest:osTmpdir()}).any());
 
 
-// Allow Cors  	
+// Allow Cors
 app.use(cors());
 
 // Enable Trust Proxy
@@ -71,28 +71,28 @@ app.enable('trust proxy', 1);
 // Auth Middleware
 // ----------------------------------------------
 app.use(function (req, res, next) {
-	
+
 	var ip     = (req.headers['x-forwarded-for'] || '').split(',')[0] || req.connection.remoteAddress.split(':').slice(-1)[0];
 	var origin = req.headers.origin;
 	var token  = req.headers.token;
 	var secret = req.headers.secret;
-	if( auth ){	  
+	if( auth ){
 		switch( auth.type ){
-	  		case 'token':	  			  		
+	  		case 'token':
 		  		polices.token(token,secret,ip,origin,function(r,data){
 		  		  if(r){
-		  		  	this.account = data;		  		 
+		  		  	this.account = data;
 				    next();
 				  } else {
 				    return res.status(401).json({status:401,msg:'Unauthorized, invalid token or missing'})
 				  }
-		  		}) 	  		
+		  		})
 			break;
 			case 'userlogin':
 				// TO-DO
-		  	break;  
+		  	break;
 		}
-	}else{ 
+	}else{
 		next();
 	}
 });
@@ -100,40 +100,48 @@ app.use(function (req, res, next) {
 // Dynamically include routes (Controller)
 // ----------------------------------------------
 fs.readdirSync(fs.realpathSync('controllers',{})).forEach(function (file) {
-    var route = require(fs.realpathSync('controllers',{}) +'/'+ file);      
+    var route = require(fs.realpathSync('controllers',{}) +'/'+ file);
     for( x in route){
         var action     = route[x];
         var uriname    = x.replace('index','');
-        var uriname    = uriname.replace(/[A-Z]/g, function(s){ return "-" + s; }).toLowerCase();     
+        var uriname    = uriname.replace(/[A-Z]/g, function(s){ return "-" + s; }).toLowerCase();
         var xfile      = file.replace('.js','') ;
-        var xfile      = xfile == 'index' && uriname.length == 0 ? xfile.replace('index','') : xfile ;   
+        var xfile      = xfile == 'index' && uriname.length == 0 ? xfile.replace('index','') : xfile ;
         var uri        = join('/',xfile,uriname);
-        if( typeof( routes[uri] ) != 'undefined' ){          
+        if( typeof( routes[uri] ) != 'undefined' ){
           app[routes[uri].method](uri,action);
         }else{
           app.all(uri,action);
-        }     
+        }
   	}
-}); 
+});
 
 // Dynamically require models (models)
 // ----------------------------------------------
-fs.readdirSync(fs.realpathSync('models',{})).forEach(function(file,i) { 
+fs.readdirSync(fs.realpathSync('models',{})).forEach(function(file,i) {
       var name            = file.replace('.js','') ;
       var modelname       = name.charAt(0).toUpperCase() + name.slice(1);
       //require file
-      this[modelname] = require(join(fs.realpathSync('models',{}),'/', file));                    
+      this[modelname] = require(join(fs.realpathSync('models',{}),'/', file));
       //attributes
       this[modelname].collection = name.toLowerCase();
-      //extendig model 
-      this[modelname] = _.extend({}, model, this[modelname]);      
+      //extendig model
+      this[modelname] = _.extend({}, model, this[modelname]);
+});
+
+// Dynamically require services
+// ----------------------------------------------
+fs.readdirSync(fs.realpathSync('services',{})).forEach(function(file,i) {
+    var name            = file.replace('.js','') ;
+    var service_name    = name.charAt(0).toUpperCase() + name.slice(1);
+    this[service_name]  = require(join(fs.realpathSync('services',{}),'/', file));
 });
 
 
 app.use(function(req, res, next) {
   var err = new Error();
   //console.log(err);
-  res.status(400).json({status:400,msg:'Bad Request'});      
+  res.status(400).json({status:400,msg:'Bad Request'});
 });
 
 global.tenant  		= { config:local, io:io, app:app };
@@ -141,10 +149,10 @@ global.tenant  		= { config:local, io:io, app:app };
 // Run server
 // ----------------------------------------------
 module.exports.run = function(options){
-	
+
 	// Run Socket Server
 	http.listen(port, dmn.bind(function () {
-	 // set port	
+	 // set port
 	 // var port = typeof options === 'undefined' ? port : options.port;
 	  console.log( (' Tenant Socket Services running at port '+port + ' ').bgGreen.black);
 	  console.log( ('--------------------------------------------------').gray);
